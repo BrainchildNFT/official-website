7ecb8768b30417ac537b45701ab8d2a69c0fa0b0
gnoose <jieli3838@gmail.com>
2021-12-23 12:46:08 +0900

    feat: initial landing page

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -13,14 +13,14 @@ export function Navbar() {
   return (<div className={navbarOpen ? "dark-background-image" : "dark-background-image pt-40"}>
     <nav className="h-65 z-10 flex px-15 sticky text-white filter drop-shadow-lg border-y border-gradient-light">
       <div className="container px-0 mx-auto flex justify-between xl:justify-left">
-        <Link href="/"><a className="flex items-center"><Image className="cursor-pointer" src="/assets/images/logo-light-large.svg" width={isMobile ? 180 : 201} height={isMobile ? 55 : 63} alt="Brainchild logo" /></a></Link>
+        <Link href="/"><a className="flex items-center"><Image className="cursor-pointer" src="/assets/images/logo/logo-light-large.svg" width={isMobile ? 180 : 201} height={isMobile ? 55 : 63} alt="Brainchild logo" /></a></Link>
         <button className="xl:hidden outline-none px-10" onClick={() => setNavbarOpen(!navbarOpen)}><Icon name="menu" color="white" size={27} /></button>
         <div className={
           "fixed xl:relative duration-300 transition-all xl:transition-none h-screen xl:h-auto xl:flex flex-col xl:flex-row xl:flex-grow w-full md:w-365 xl:bg-opacity-0 top-0 justify-start xl:justify-between items-start xl:items-center drop-shadow-lg" +
           (navbarOpen ? " left-0 ease-out-in light-background-image" : " -left-800 xl:left-0 ease-in-out")
         }>
           <div className="flex w-full xl:hidden justify-between py-30 dark-background-image h-65 px-20 xl:px-0 ">
-            <Link href="/"><a className="flex xl:hidden items-center"><Image className="cursor-pointer" src="/assets/images/logo-light-large.svg" width={isMobile ? 180 : 201} height={isMobile ? 55 : 63} alt="Brainchild logo" /></a></Link>
+            <Link href="/"><a className="flex xl:hidden items-center"><Image className="cursor-pointer" src="/assets/images/logo/logo-light-large.svg" width={isMobile ? 180 : 201} height={isMobile ? 55 : 63} alt="Brainchild logo" /></a></Link>
             <button className="px-10 flex items-center" onClick={() => setNavbarOpen(false)}><Icon name="close" color="white" size={25} /></button>
           </div>
           <div className="w-full xl:hidden px-10 py-20">
--- components/ui-kit/icon/icons.ts
+++ components/ui-kit/icon/icons.ts
@@ -57,6 +57,16 @@ export const brainchildIcons = {
       <path d="M21 21L16.65 16.65" stroke="${getColor(color)}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
   `,
+  down: (size = 330, color?: string) => `
+    <svg width="${size}" height="${size}" fill="${getColor(color)}" viewBox="0 0 330 330" xmlns="http://www.w3.org/2000/svg">
+        <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393  c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393  s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z" stroke="${getColor(color)}"/>
+    </svg>
+  `,
+  up: (size = 330, color?: string) => `
+    <svg width="${size}" height="${size}" fill="${getColor(color)}" viewBox="0 0 330 330" xmlns="http://www.w3.org/2000/svg">
+        <path id="XMLID_224_" d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394  l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393  C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z" stroke="${getColor(color)}"/>
+    </svg>
+  `,
   check: (size = 24, color?: string) => `
     <svg width="${size}" height="${size}" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M20 6L9 17L4 12" stroke="${getColor(color)}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>

4d17ff1db39e2a0a266f0f554c72dc3fd56ec7a4
gnoose <jieli3838@gmail.com>
2022-04-27 06:37:10 +0900

    feat/update-mainnet

--- next.config.js
+++ next.config.js
@@ -8,9 +8,9 @@ module.exports = withPWA({
   },
   compress: false, // brotli compress rather than default gzip support of next.js
   env: {
-    contractAddress: '0xa25c6b358B9684C21605265940484B2C9558798a',
-    ownerAddress: '0x9e16e0C678DbB15aFe8a1159d03F8970e41EE882',
-    rpcURI: 'https://speedy-nodes-nyc.moralis.io/a0dd13b0091772cbf75a91db/eth/rinkeby',
+    contractAddress: '0x59d40F2CAD16ecA53996706c7f4895a4A9d0183b',
+    ownerAddress: '0x113EDED148717B21140ed7E1A92D2A677B241D34',
+    rpcURI: 'https://speedy-nodes-nyc.moralis.io/a0dd13b0091772cbf75a91db/eth/mainnet',
     api: 'https://api.brainchildnft.com',
     infuraId: '41ef39b8a23943439d5c1d071593c52f',
     mintCount: 3,
